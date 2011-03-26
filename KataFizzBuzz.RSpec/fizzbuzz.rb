44d0c9f739556d67f9e6e0eed676c51b03df88f3
hoyer <mhoyer@pixelplastic.de>
2011-03-26 15:01:19 +0100

    * simplified FizzBuzz#translate to match ruby style

--- KataFizzBuzz.RSpec/fizzbuzz.rb
+++ KataFizzBuzz.RSpec/fizzbuzz.rb
@@ -1,16 +1,8 @@
 class FizzBuzz
 	def translate(number)
-		if number % 15 == 0
-			return "fizzbuzz"
-		end
-
-		if number % 5 == 0
-			return "buzz"
-		end
-
-		if number % 3 == 0
-			return "fizz"
-		end
+		return "fizzbuzz" if number % 15 == 0
+		return "buzz" if number % 5 == 0
+		return "fizz" if number % 3 == 0
 
 		return number.to_s
 	end
